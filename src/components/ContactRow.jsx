7e33cff814a8d2950e9c72fd1ea48aa20b30a9cd
avithew <avithew@gmail.com>
2024-09-21 23:40:25 -0600

    set up table

---
+++ src/components/ContactRow.jsx
@@ -0,0 +1,11 @@
+function ContactRow({ name, email, phone }) {
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{email}</td>
+      <td>{phone}</td>
+    </tr>
+  );
+}
+
+export default ContactRow;
